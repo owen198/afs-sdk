2aef01d1e2b6b11c9b51ebb05885498b216f4602
root <cfleu198@gmail.com>
2018-04-04 00:36:36 +0800

    switch port from 666 to 80

--- FileServer.py
+++ FileServer.py
@@ -29,4 +29,4 @@ def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=666)
+    app.run(debug=True, host="0.0.0.0", port=80)
