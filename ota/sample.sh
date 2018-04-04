e65452055a5c414bad4df0761faddac4c07978db
oliu@iii.org.tw <oliu@iii.org.tw>
2018-04-05 02:11:39 +0800

    add a sample code

---
+++ ota/sample.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+HOST=owenotahubtestu32.iii-cflab.com
+
+touch model.pkl
+touch model.bat
+
+curl -X POST -F file=@model.pkl http://$HOST/upload
+curl -X POST -F file=@model.bat http://$HOST/upload
+
+curl http://$HOST/pack
+curl http://$HOST/download
