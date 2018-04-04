7d9c88d91f013d37aead27325ced67dbec834a94
root <cfleu198@gmail.com>
2018-04-04 13:55:24 +0800

    finish upload function

--- ota/FileServer.py
+++ ota/FileServer.py
@@ -10,8 +10,10 @@ app = Flask(__name__)
 STATUS_CODE = "statuscode"
 ALLOWED_EXTENSIONS = ['pkl', 'bat']
 
-PATH_MODEL="/root/afs-sdk/ota/model/"
-NAME_MODEL="model.zip"
+PATH_ROOT = "/root/afs-sdk/"
+PATH_OTA = PATH_ROOT + "ota/"
+PATH_MODEL = PATH_OTA + "model/"
+NAME_MODEL = "model.zip"
 
 @app.route('/pack', methods=['GET'])
 def pack():
@@ -29,7 +31,7 @@ def pack():
 @app.route('/download', methods=['GET'])
 def download():
     try:
-
+        # if model.zip exists, return file to client
         if os.path.isfile(PATH_MODEL+NAME_MODEL):
             return send_file(PATH_MODEL+NAME_MODEL, attachment_filename=NAME_MODEL)
         else:
@@ -44,23 +46,41 @@ def download():
 def upload():
     try:
         file = request.files['file']
-        file_path = request.form['file_path']  
+        #file_path = request.form['file_path'] 
+        file_path = PATH_MODEL
+
+        # if file extension is allowable, clean pervious files and upload to PATH_MODEL 
         if file and allowed_file(file.filename):
 
-            #TODO: clean pervious files
+            #clean pervious files
+            clean_pervious(PATH_MODEL, ".pkl")
+            clean_pervious(PATH_MODEL, ".bat")
+
 	    filename = os.path.join(file_path, file.filename)
             file.save(filename)
 
             return jsonify({STATUS_CODE:200}), 200
+        else:
+            return jsonify({STATUS_CODE:213}), 200
     except ValueError:
         return jsonify({STATUS_CODE:211}), 400
     except IOError:
         return jsonify({STATUS_CODE:212}), 400
+    #except Exception, e: print e
     except :
 	return jsonify({STATUS_CODE:500}), 500
 
 def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def clean_pervious(dir_name=PATH_ROOT, extensions=".just_give_one"):
+    test = os.listdir(dir_name)
+
+    for item in test:
+        if item.endswith(extensions):
+            os.remove(os.path.join(dir_name, item))
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
--- ota/README.md
+++ ota/README.md
@@ -28,7 +28,13 @@
 
     curl -X POST -F file=@model.pkl -F file_path=model http://${HOST}/upload
 
-
+|Status Code           | Description                                                         |
+|----------------------|---------------------------------------------------------------------|
+|200                   | Upload successed                                                    |
+|211                   | flask.file() ValueError, usually give wrong argv                    |
+|212                   | flask.file() IOError, please check PATH_MODEL at server site        |
+|213                   | File extension does not allowed, only accept `.zip` and `.bat`      |
+|500                   | flask.file() execptions                                             |
 
 
 cf push owen_otahub_test_u32 --docker-image cfleu198/ubuntu32 -c "python /afs-sdk/ota/FileServer.py" -k 2g -m 2g
