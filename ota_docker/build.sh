ed53245bbe41cde5c8a4c4c7603a51628a434d5a
root <cfleu198@gmail.com>
2018-04-04 20:59:15 +0800

    add build.sh

---
+++ ota_docker/build.sh
@@ -0,0 +1,5 @@
+
+#docker build -no-cache -t="cfleu198/ubuntu32" .
+docker build -t="cfleu198/ubuntu32" .
+docker push cfleu198/ubuntu32
+#cf push owen_otahub_test_u32 --docker-image cfleu198/ubuntu32 -c "python /afs-sdk/ota/FileServer.py" -k 2g -m 2g
